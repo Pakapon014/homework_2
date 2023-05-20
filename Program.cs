2ddf20aa7da591bea9d99bfaab1917f79c60149a
pakapon <pakapon.krin@kmutt.ac.th>
2023-05-20 11:21:25 +0700

    add1

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            Console.Write("Enter half of the DNA sequence: ");
+            string halfDNASequence = Console.ReadLine();
+
+            if (IsValidSequence(halfDNASequence))
+            {
+                Console.WriteLine("Current half DNA sequence: " + halfDNASequence);
+                Console.Write("Do you want to replicate it? (Y/N): ");
+                char response = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (response == 'Y' || response == 'y')
+                {
+                    string replicatedSequence = ReplicateSeqeunce(halfDNASequence);
+                    Console.WriteLine("Replicated half DNA sequence: " + replicatedSequence);
+                }
+                else if (response == 'N' || response == 'n')
+                {
+                    // Skip replication
+                }
+                else
+                {
+                    Console.WriteLine("Please input Y or N.");
+                    continue;
+                }
+            }
+            else
+            {
+                Console.WriteLine("That half DNA sequence is invalid.");
+            }
+
+            Console.Write("Do you want to process another sequence? (Y/N): ");
+            char continueResponse = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (continueResponse == 'N' || continueResponse == 'n')
+            {
+                continueProgram = false;
+            }
+            else if (continueResponse != 'Y' && continueResponse != 'y')
+            {
+                Console.WriteLine("Please input Y or N.");
+            }
+        }
+    }
+
+    static bool IsValidSequence(string halfDNASequence)
+    {
+        foreach (char nucleotide in halfDNASequence)
+        {
+            if (!"ATCG".Contains(nucleotide))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static string ReplicateSeqeunce(string halfDNASequence)
+    {
+        string result = "";
+        foreach (char nucleotide in halfDNASequence)
+        {
+            result += "TAGC"["ATCG".IndexOf(nucleotide)];
+        }
+        return result;
+    }
+}
